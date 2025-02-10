d90dcbb81149dc2151e24955435f59924073876d
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2025-02-10 07:30:07 -0300

    acho que completado

--- app/relatorio/page.jsx
+++ app/relatorio/page.jsx
@@ -18,7 +18,7 @@ const pegaFornecedores = async () => {
 
 const pegaProdutos = async () => {
     try {
-        const resp = await fetch(`${url}/api/produto`, { next: { revalidate: 10 } });
+        const resp = await fetch(`${url}/api/produto`, { next: { revalidate: 1 } });
         if (!resp.ok) throw new Error("Erro ao buscar produtos");
         const data = await resp.json();
         data.sort((a, b) => a.nomeDoProduto.localeCompare(b.nomeDoProduto));
